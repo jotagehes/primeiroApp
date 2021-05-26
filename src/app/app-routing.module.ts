d8d0b9dbc524585b206a6988954404e47f7f8f64
jotagehes <62728338+jotagehes@users.noreply.github.com>
2021-05-25 22:07:47 -0300

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
+    path: 'home',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'amigos',
+    loadChildren: () => import('./amigos/amigos.module').then( m => m.AmigosPageModule)
+  },
+  {
+    path: 'viagens',
+    loadChildren: () => import('./viagens/viagens.module').then( m => m.ViagensPageModule)
   }
 ];
 
